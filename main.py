2901c4930aee57f35862a86166b10a0c08819ea6
kelvinforson <KelvinForson2023@u.northwestern.edu>
2023-01-20 16:57:36 -0600

    sketch of code structure

---
+++ main.py
@@ -0,0 +1,44 @@
+# awardToNomineesMap maps award_name to a list of nominees for that award
+awardToNomineesMap = {}
+
+# winnerToAwardMap maps award_name to the winner of the award
+winnerToAwardMap = {} 
+
+def getWinner(award_name):
+    """
+        get winner of an award from the list of nominees of that award using the awardToNomineesMap
+    """
+    return ""
+
+def getWinners(awards_list):
+    """
+        aggregate the winners from each award name 
+    """
+    return ""
+
+
+def getAwardNominees():
+    """
+        gets all the nominees of a given an award names
+    """
+    return ""
+
+
+def getAwardCategories(awards_ceremony_name, tweets):
+    """
+        gets all the award categories from given tweets and given the name of awards ceremony
+    """
+    return ""
+
+def getHosts(awards_ceremony_name, tweets):
+    """
+        gets all the hosts for the given awards_ceremony_name from the tweets
+    """
+    keywords = [] # list of words that can identify the purpose
+    return ""
+
+
+
+
+def main():
+    return 0
---
+++ utils.py
@@ -0,0 +1,7 @@
+def getTweetsTexts(tweets):
+    """
+        getTweetsTexts returns the list of texts from each tweet and creates a list of them
+        This is particulary useful since we will be reading texts every time in each function
+
+    """
+    return "UnImplemented"
