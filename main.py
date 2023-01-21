cf845708cf2afc87d5a42654b03a829f1603f9b0
walmonte6 <willyalmonte2023@u.northwestern.edu>
2023-01-21 16:45:41 -0600

    started on winners, did utils, wrote some thots down

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
+import utils as u
+import re
+
 # awardToNomineesMap maps award_name to a list of nominees for that award
 awardToNomineesMap = {}
 
 # winnerToAwardMap maps award_name to the winner of the award
 winnerToAwardMap = {} 
 
-def getWinner(award_name):
+# map award to winners
+awardToWinner = {}
+
+def getWinner(tweets, pattern, award_name):
     """
         get winner of an award from the list of nominees of that award using the awardToNomineesMap
     """
-    return ""
+    nominees = awardToNomineesMap[award_name]
+    voting = {nominee : 0 for nominee in nominees}
+    for tweet in tweets:
+        matches =  pattern.match(tweet)
 
-def getWinners(awards_list):
+        if matches and matches.group(1) in nominees and award_name in matches.group(2):
+            winner = matches.group(1)
+            voting[winner] += 1
+
+    voted_winner = max(voting, key=voting.get)
+    return voted_winner
+
+def getWinners(tweets, awards_list):
     """
         aggregate the winners from each award name 
+        
     """
-    return ""
+    pattern = re.compile(r"(.*)[won|wins](.*)?")
+    for award in awards_list:
+        awardToWinner[award] = getWinner(tweets, pattern, award)
 
 
-def getAwardNominees():
+def getAwardNominees(award_name):
     """
         gets all the nominees of a given an award names
     """
@@ -41,4 +60,10 @@ def getHosts(awards_ceremony_name, tweets):
 
 
 def main():
+    tweets = u.getTweets("gg2013.json")
+    
+    
     return 0
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,7 +1,17 @@
+import json
+
 def getTweetsTexts(tweets):
     """
         getTweetsTexts returns the list of texts from each tweet and creates a list of them
         This is particulary useful since we will be reading texts every time in each function
 
     """
-    return "UnImplemented"
+    texts = []
+    for tweet in tweets:
+        texts.append(tweet["text"].lower())
+    return texts
+
+def getTweets (file_name):
+    file = open(file_name)
+    tweets = json.load(file)
+    return getTweetsTexts(tweets)
