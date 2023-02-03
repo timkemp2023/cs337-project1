0773fa61a22b8fac47a636cc890ed5674339b070
Chris Kemp <timkemp2023@u.northwestern.edu>
2023-02-03 15:46:53 -0600

    constants file

---
+++ constants.py
@@ -0,0 +1,35 @@
+AWARD_NAMES_SET = {
+    "best screenplay - motion picture": set({"best", "picture", "motion"}),
+    "best director - motion picture": set({"best", "director"}),
+    "best performance by an actress in a television series - comedy or musical": set({"best", "actor", "tv", "comedy", "musical"}),
+    "best foreign language film": set({"best", "foreign", "film"}),
+    "best performance by an actor in a supporting role in a motion picture": set({"best", "actor", "supporting", "picture"}),
+    "best performance by an actress in a supporting role in a series, mini-series or motion picture made for television": set({"best", "actress", "supporting", "tv"}),
+    "best motion picture - comedy or musical": set({"best", "picture", "comedy", "musical"}),
+    "best performance by an actress in a motion picture - comedy or musical": set({"best", "acress", "picture", "comedy", "musical"}),
+    "best mini-series or motion picture made for television": set({"best", "tv", "picture"}),
+    "best original score - motion picture": set({"best", "original", "score"}),
+    "best performance by an actress in a television series - drama": set({"best", "actress", "tv", "drama"}),
+    "best performance by an actress in a motion picture - drama": set({"best", "actress", "picture", "drama"}),
+    "cecil b. demille award": set({"cecil"}),
+    "best performance by an actor in a motion picture - comedy or musical": set({"best", "actor", "picture", "comedy", "musical"}),
+    "best motion picture - drama": set({"best", "picture", "drama"}),
+    "best performance by an actor in a supporting role in a series, mini-series or motion picture made for television": set({"best", "actor", "supporting", "tv", "picture"}),
+    "best performance by an actress in a supporting role in a motion picture": set({"best", "supporting", "actress", "picture"}),
+    "best television series - drama": set({"best", "tv", "drama"}),
+    "best performance by an actor in a mini-series or motion picture made for television": set({"best", "actor", "tv", "picture"}),
+    "best performance by an actress in a mini-series or motion picture made for television": set({"best", "actress", "tv", "picture"}),
+    "best animated feature film": set({"best", "animated", "film"}),
+    "best original song - motion picture": set({"best", "original", "song"}),
+    "best performance by an actor in a motion picture - drama": set({"best", "actor", "picture", "drama"}),
+    "best television series - comedy or musical": set({"best", "tv", "series", "comedy", "musical"}),
+    "best performance by an actor in a television series - drama": set({"best", "actor", "tv", "drama"}),
+    "best performance by an actor in a television series - comedy or musical": set({"best", "tv", "actor", "comedy", "musical"})
+
+
+
+
+
+
+
+}
